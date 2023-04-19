f9baa78b54e572df63bd4fec617bdbebc846a463
rwitzel <rwitzel.witzel@gmx.de>
2023-04-19 08:35:30 +0200

    valid_categories

--- process.py
+++ process.py
@@ -29,6 +29,18 @@ categories = read_categories()
 flat_list = [item for sublist in categories.values() for item in sublist]
 filtered_words_list = [word for word in words if word not in flat_list]
 
+valid_categories = []
+for category, words_in_category in categories.items():
+    valid_words = [word for word in words_in_category if word in words]
+    valid_categories.append(category + ": " + ", ".join(valid_words))
+with open("valid_categories.txt", "w") as f:
+    f.write("\n".join(valid_categories))
+
+
+#for word in flat_list:
+#    if word not in words:
+#        raise Exception(f"chatgpt word '{word}' is not in list")
+
 def write_filtered_words(filtered_words_list):
     with open("filtered_words.txt", "w") as filtered_words_file:
         filtered_words_file.write(", ".join(filtered_words_list))
